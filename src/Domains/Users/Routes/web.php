790f7843ac58c27cef131c7b78717bf950e40fdb
Danny Lisman <>
2026-01-21 18:37:42 +0100

    Naaah

--- src/Domains/Users/Routes/web.php
+++ src/Domains/Users/Routes/web.php
@@ -2,10 +2,6 @@
 declare(strict_types=1);
 
 use Domains\Users\Repositories\UsersQueryRepository;
-use Domains\Users\Services\Queries\BrokenNonReadonlyQueryService;
-use Domains\Users\Services\Queries\BrokenNonReadonlyResultService;
-use Domains\Users\Services\Queries\BrokenWrongSuffixQueryService;
-use Domains\Users\Services\Queries\BrokenWrongSuffixResultService;
 use Domains\Users\Services\Queries\ListUsersQuery;
 use Domains\Users\Services\Queries\ListUsersService;
 use Lilly\Http\DomainRouter;
@@ -24,33 +20,5 @@ return function (DomainRouter $router): void {
         $result = $service->handle(new ListUsersQuery($name, $limit));
 
         return Response::json($result->items);
-    });
-
-    $router->get('/users/broken/non-readonly-query', function (): Response {
-        $service = new BrokenNonReadonlyQueryService();
-        $service->run();
-
-        return Response::json(['ok' => true]);
-    });
-
-    $router->get('/users/broken/wrong-suffix-query', function (): Response {
-        $service = new BrokenWrongSuffixQueryService();
-        $service->run();
-
-        return Response::json(['ok' => true]);
-    });
-
-    $router->get('/users/broken/non-readonly-result', function (): Response {
-        $service = new BrokenNonReadonlyResultService();
-        $service->run();
-
-        return Response::json(['ok' => true]);
-    });
-
-    $router->get('/users/broken/wrong-suffix-result', function (): Response {
-        $service = new BrokenWrongSuffixResultService();
-        $service->run();
-
-        return Response::json(['ok' => true]);
     });
 };
