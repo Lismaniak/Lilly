c32271b26590afa3f22f633527df09d0f7b82483
Danny Lisman <>
2026-01-22 14:52:41 +0100

    Removed fluff

--- src/Domains/Users/Components/AddUser/Component.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Domains\Users\Components\AddUser;
-
-final class Component
-{
-}
--- src/Domains/Users/Components/AddUser/Routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use Lilly\Http\DomainRouter;
-
-return function (DomainRouter $router): void {
-};
