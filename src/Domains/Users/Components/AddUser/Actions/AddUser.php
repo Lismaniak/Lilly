78bfb75a2927d48860730ea8b635e0aee9ddcf67
Lisman <d.l@live.nl>
2026-01-22 15:57:52 +0100

    Return result dto from add user action

--- src/Domains/Users/Components/AddUser/Actions/AddUser.php
+++ src/Domains/Users/Components/AddUser/Actions/AddUser.php
@@ -5,13 +5,29 @@ namespace Domains\Users\Components\AddUser\Actions;
 
 use Domains\Users\Repositories\UsersCommandRepository;
 use Domains\Users\Services\Commands\CreateUserData;
-use Domains\Users\Services\Commands\CreateUserResult;
 use Domains\Users\Services\Commands\CreateUserService;
 use Lilly\Database\Orm\Orm;
+use Lilly\Dto\ResultDto;
+use Lilly\Validation\ArrayValidator;
+
+final readonly class AddUserInput
+{
+    public string $name;
+
+    public function __construct(string $name)
+    {
+        $data = ArrayValidator::map(
+            ['name' => $name],
+            ['name' => ['required', 'string', 'max:255', 'non_empty']]
+        );
+
+        $this->name = $data['name'];
+    }
+}
 
 final class AddUser
 {
-    public function handle(Orm $orm, AddUserInput $input): CreateUserResult
+    public function handle(Orm $orm, AddUserInput $input): ResultDto
     {
         $repository = new UsersCommandRepository($orm);
         $service = new CreateUserService($repository);
